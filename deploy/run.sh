75250332d5a4beb4568dd9a47e7ba48da5e893a2
Gage Vander Clay <gvanderclay@gmail.com>
2018-12-16 10:37:05 -0500

    mor tests

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,7 +6,8 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
 fi
 
-aws s3 cp s3://vc-chat/bridge-migration.csv test.csv
+# aws s3 cp s3://vc-chat/bridge-migration.csv test.csv
+aws opsworks describe-my-user-profile
 
 cd ~/node
 pm2 start npm -n vc-chat -- start 
