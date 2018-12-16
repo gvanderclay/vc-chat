750faa9fa5cf3283f163cec7ff0ffb4ad1de08f8
Gage Vander Clay <gvanderclay@gmail.com>
2018-12-16 10:21:58 -0500

    Remove profile reference from s3

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,7 +6,7 @@ if [ ! -z "$DEPLOYMENT_GROUP_NAME" ]; then
  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
 fi
 
-aws s3 cp s3://vc-chat/bridge-migration.csv test.csv --profile personal
+aws s3 cp s3://vc-chat/bridge-migration.csv test.csv
 
 cd ~/node
 pm2 start npm -n vc-chat -- start 
