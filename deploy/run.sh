f5b06dd7cf6faebc41d6c9477e88e3d093257e96
Gage Vander Clay <gvanderclay@gmail.com>
2018-12-28 14:02:27 -0500

    S3 debug

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,7 +6,7 @@ if [ -n "$DEPLOYMENT_GROUP_NAME" ]; then
  export NODE_ENV=$DEPLOYMENT_GROUP_NAME
 fi
 
-aws s3 cp s3://vc-chat/bridge-migration.csv test.csv
+aws s3 cp s3://vc-chat/bridge-migration.csv test.csv --debug
 
 cd ~/node
 pm2 start npm -n vc-chat -- start 
